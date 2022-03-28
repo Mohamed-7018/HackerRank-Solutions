7df3a312963f87c7b3572e13aee7e92e60140b69
Mohamed-7018 <80456446+Mohamed-7018@users.noreply.github.com>
2022-03-28 08:15:40 +0200

    28/3/2022 first time

---
+++ python/Strings/Find a string.py
@@ -0,0 +1,16 @@
+def count_substring(string, sub_string):
+     count = 0
+     i= string.find(sub_string)
+     while i !=-1 :
+        count += 1
+        i = string.find(sub_string, i+1)
+        
+     return count
+    
+    
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
